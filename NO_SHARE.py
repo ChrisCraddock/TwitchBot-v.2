292cf27e983752f8a415e616d4f1da9d5ea72f5b
ChrisCraddock <30630937+ChrisCraddock@users.noreply.github.com>
2021-10-07 02:07:58 -0400

    Create NO_SHARE.py

---
+++ NO_SHARE.py
@@ -0,0 +1,6 @@
+BOT_NAME = "<bot name>"
+CHNL_NAME = "<YOUR streaming channel name>"
+APP_ID = '<app id of the bot>'
+APP_SCRT = '<app secret of the bot>'
+BOT_ID = "<bot id of the bot>"
+BOT_TOKEN = "<bot token of the bot>"
